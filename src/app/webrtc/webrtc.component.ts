df3cc67fa572d0a96a0d5fcb25ecd6326a6d0c01
Samuel Marks <samuelmarks@gmail.com>
2017-09-30 17:31:44 +1000

    2/4 implementing WebRTC

--- src/app/webrtc/webrtc.component.ts
+++ src/app/webrtc/webrtc.component.ts
@@ -8,10 +8,16 @@ import { AlertsService } from '../alerts/alerts.service';
   styleUrls: ['./webrtc.component.css']
 })
 export class WebrtcComponent implements AfterViewInit {
-  @Input() audioSource;
-  @Input() videoSource;
-  selectedValue: any;
-  @Input() mediaDeviceInfos: MediaDeviceInfo[] = [];
+  /*@Input() audioSource;
+  @Input() videoSource;*/
+  @Input() video: HTMLVideoElement;
+  selectedCam: string;
+  selectedMic: string;
+  cams: MediaDeviceInfo[] = [];
+  mics: MediaDeviceInfo[] = [];
+  mediaDeviceInfos: MediaDeviceInfo[] = [];
+  deviceId2Label: {[index: string]: string} = {};
+  deviceId2Device: {[index: string]: MediaDeviceInfo} = {};
 
 
   constructor(private alertsService: AlertsService) { }
@@ -24,7 +30,7 @@ export class WebrtcComponent implements AfterViewInit {
     navigator.mediaDevices
       .enumerateDevices()
       .then(this.gotDevices.bind(this))
-      .then(this.getStream.bind(this))
+      // .then(this.getStream.bind(this))
       .catch(this.alertsService.add.bind(this.alertsService));
 
     /*audioSelect.onchange = getStream;
@@ -32,8 +38,22 @@ export class WebrtcComponent implements AfterViewInit {
 
   }
 
-  gotDevices(deviceInfos) {
+  gotDevices(deviceInfos: MediaDeviceInfo[]) {
     this.mediaDeviceInfos = deviceInfos;
+    this.mediaDeviceInfos.forEach(device => {
+        if (device.kind === 'audioinput')
+          this.mics.push(device);
+        else if (device.kind === 'videoinput')
+          this.cams.push(device);
+        else
+          console.warn(`Unexpected: ${JSON.stringify(device)}`);
+        this.deviceId2Label[device.deviceId] = {
+          audioinput: `microphone: ${device.deviceId}`,
+          videoinput: `camera: ${device.deviceId}`
+        }[device.kind] || `[${device.kind}]: ${device.groupId} - ${device.deviceId}`;
+        this.deviceId2Device[device.deviceId] = device;
+      }
+    );
     console.info('deviceInfos =', deviceInfos, ';');
     /*
     for (let i = 0; i !== deviceInfos.length; ++i) {
@@ -56,6 +76,44 @@ export class WebrtcComponent implements AfterViewInit {
   }
 
   getStream() {
+    /*const constraints = {
+      audio: {
+        optional: [{
+          sourceId: this.selectedMic
+        }]
+      },
+      video: {
+        optional: [{
+          sourceId: this.selectedCam
+        }]
+      }
+    };*/
+
+    const mic: MediaDeviceInfo = this.deviceId2Device[this.selectedMic];
+    const cam: MediaDeviceInfo = this.deviceId2Device[this.selectedCam];
+    console.info('getStream::mic =', mic, ';');
+    console.info('getStream::cam =', cam, ';');
+    const constraints: MediaStreamConstraints = {
+      audio: {
+        advanced: [{
+          deviceId: mic.deviceId,
+          groupId: mic.groupId
+        }]
+      },
+      video: {
+        advanced: [{
+          deviceId: cam.deviceId,
+          groupId: cam.groupId
+        }]
+      }
+    };
+
+    navigator
+      .mediaDevices
+      .getUserMedia(constraints)
+      .then(this.gotStream.bind(this))
+      .catch(this.alertsService.add.bind(this.alertsService));
+
     /*if (window.stream) {
       window
         .stream
@@ -85,7 +143,8 @@ export class WebrtcComponent implements AfterViewInit {
     */
   }
 
-  gotStream(stream) {
+  gotStream(stream: MediaStream) {
+    this.video.srcObject = stream;
     /*window.stream = stream; // make stream available to console
     videoElement.srcObject = stream;*/
   }
